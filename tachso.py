85cd7291c7233c1a5972516338f6700912dad85f
Leducphat1234 <Leducphat1234>
2025-06-21 01:06:16 +0700

    small bug

--- tachso.py
+++ tachso.py
@@ -4,8 +4,8 @@ sys.stdout = open("tachso.out", "w")
 
 n = int(input())
 cnt = 0
-for b in range(1, n-2):
-    for c in range(1, n-2):
+for b in range(1, n-1):
+    for c in range(1, n-1):
         a = n - b - c
         if a > 1 and b % a == 0 and c % a == 0:
             cnt += 1
